7bfb0fef423e39a14e436b295a368ff39847505d
Daniel X. Du <du.daniel.x@gmail.com>
2022-07-08 18:41:37 -0400

    Fixed image loading scroll.
    Can adapt this + slideshow function in order to make news page?

--- scripts/scroller/scroller.js
+++ scripts/scroller/scroller.js
@@ -14,12 +14,46 @@ const imageSources = [ // Put your image sources in this list
 './published_code_images/20180704_092921.jpg',
 './published_code_images/LumineBruh.gif',
 './published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
 './published_code_images/LumineBruh.gif'
 ];
 
+let testele = document.createElement("img");
+testele.className = "photoImg";
+document.getElementById("photoSec").appendChild(testele);
 numLoaded = 0;
+win_height_size = window.innerWidth;
+win_width_size = window.innerHeight;
+img_height_size = testele.clientHeight;
+img_width_size = testele.clientWidth;
+testele.remove();
 
-for (let i = 0; i < imageSources.length-1; i++) // Change this 5 to however many images you have (or use for ... in) 
+numImgPerRow = Math.floor(win_width_size / img_width_size);
+numRows = Math.ceil(win_height_size / img_height_size);
+numImgLoad = numRows * numImgPerRow;
+
+for (let i = 0; i < numImgLoad; i++) // Change this 5 to however many images you have (or use for ... in) 
 { 
     let divele = document.createElement("div");
     divele.className = "photoCard";
@@ -35,30 +69,44 @@ for (let i = 0; i < imageSources.length-1; i++) // Change this 5 to however many
 
 // Check to see if scrolling near bottom of page; load more photos
 window.addEventListener('scroll', () => {
-    if (window.scrollY + window.innerHeight >= document.body.offsetHeight - 1000) 
+    if (window.scrollY + window.innerHeight >= document.body.offsetHeight - img_height_size) 
     {
-        console.log("sup");
         getPhotos();
-        console.log("supafter");
     }
 });
 
 function getPhotos()
 {
-    for (let i = 0; i < imageSources.length-1; i++) // Change this 5 to however many images you have (or use for ... in) 
-    { 
-        if (numLoaded >= imageSources.length)
-        {
-            let divele = document.createElement("div");
-            divele.className = "photoCard";
-            let imgele = document.createElement("img");
-            imgele.className = "photoImg";
-            imgele.setAttribute("src", imageSources[i]);
-            divele.appendChild(imgele)
-            document.getElementById("photoSec").appendChild(divele);
-            numLoaded++;
+    
+    if (numLoaded <= imageSources.length-1)
+    {
+        // reload img sizes
+        let testele = document.createElement("img");
+        testele.className = "photoImg";
+        document.getElementById("photoSec").appendChild(testele);
+        win_height_size = window.innerWidth;
+        win_width_size = window.innerHeight;
+        img_height_size = testele.clientHeight;
+        img_width_size = testele.clientWidth;
+        testele.remove();
+
+        numImgPerRow = Math.floor(win_width_size / img_width_size);
+        console.log(numImgPerRow);
+
+        for (let i = 0; i < numImgPerRow; i++) // Change this 5 to however many images you have (or use for ... in) 
+        { 
+            if (numLoaded <= imageSources.length-1)
+            {
+                let divele = document.createElement("div");
+                divele.className = "photoCard";
+                let imgele = document.createElement("img");
+                imgele.className = "photoImg";
+                imgele.setAttribute("src", imageSources[numLoaded]);
+                divele.appendChild(imgele)
+                document.getElementById("photoSec").appendChild(divele);
+                numLoaded++;
+            }
         }
     }
-    page++;
-    loading.classList.remove("show");
+    //loading.classList.remove("show");
 };
