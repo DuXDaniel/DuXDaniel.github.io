40db91fe5ce7e845edea6ce64c11c42a9ba3ba4d
Daniel X. Du <du.daniel.x@gmail.com>
2022-07-08 19:52:19 -0400

    Added news scroller

---
+++ scripts/news_scroller.js
@@ -0,0 +1,115 @@
+// load csv data
+// load dirImages
+// load curCount
+const imageSources = [ // Put your image sources in this list
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif',
+'./published_code_images/AmePls.gif',
+'./published_code_images/20180704_092921.jpg',
+'./published_code_images/LumineBruh.gif'
+];
+
+let testele = document.createElement("div");
+testele.className = "newsCard";
+let testimg = document.createElement("img");
+testimg.className = "newsImg";
+testimg.setAttribute("src", imageSources[0]);
+testele.appendChild(testimg);
+document.getElementById("newsSec").appendChild(testele);
+numLoaded = 0;
+win_height_size = window.innerWidth;
+win_width_size = window.innerHeight;
+img_height_size = testele.clientHeight;
+img_width_size = testele.clientWidth;
+testimg.remove();
+testele.remove();
+
+numRows = Math.ceil(win_height_size / img_height_size);
+console.log(numRows);
+
+for (let i = 0; i < numRows; i++) // Change this 5 to however many images you have (or use for ... in) 
+{ 
+    let divele = document.createElement("div");
+    divele.className = "newsCard";
+    let imgele = document.createElement("img");
+    imgele.className = "newsImg";
+    imgele.setAttribute("src", imageSources[i]);
+    divele.appendChild(imgele);
+    document.getElementById("newsSec").appendChild(divele);
+    numLoaded++;
+}
+
+// tweak num loaded to load based on size of screen etc etc etc
+
+// Check to see if scrolling near bottom of page; load more photos
+window.addEventListener('scroll', () => {
+    if (window.scrollY + window.innerHeight >= document.body.offsetHeight - img_height_size) 
+    {
+        getPhotos();
+    }
+});
+
+function getPhotos()
+{
+    
+    if (numLoaded <= imageSources.length-1)
+    {
+        // reload img sizes
+        let testele = document.createElement("div");
+        testele.className = "newsCard";
+        let testimg = document.createElement("img");
+        testimg.className = "newsImg";
+        testimg.setAttribute("src", imageSources[0]);
+        testele.appendChild(testimg);
+        document.getElementById("newsSec").appendChild(testele);
+        win_height_size = window.innerWidth;
+        win_width_size = window.innerHeight;
+        img_height_size = testele.clientHeight;
+        img_width_size = testele.clientWidth;
+        testimg.remove();
+        testele.remove();
+        
+        if (numLoaded <= imageSources.length-1)
+        {
+            let divele = document.createElement("div");
+            divele.className = "newsCard";
+            let imgele = document.createElement("img");
+            imgele.className = "newsImg";
+            imgele.setAttribute("src", imageSources[numLoaded]);
+            divele.appendChild(imgele);
+            document.getElementById("newsSec").appendChild(divele);
+            numLoaded++;
+        }
+    }
+    //loading.classList.remove("show");
+};
